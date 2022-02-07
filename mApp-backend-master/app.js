16165bf96497cb73f58e1b4fdf85974bfe56abbf
Phuthipong <gukatza08147@gmail.com>
2022-02-08 02:06:51 +0700

    mobileapp 2

---
+++ mApp-backend-master/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const cors = require("cors");
+
+const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const usersRouter_mongo = require("./routes/mongo/users");
+const productsRouter_mongo = require("./routes/mongo/products");
+const customersRouter_mongo = require("./routes/mongo/customers");
+
+var app = express();
+
+app.use(cors());
+
+mongoose.connect("mongodb+srv://admin:0882145113za@cluster0.eab3s.mongodb.net/rmutl?retryWrites=true&w=majority");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use("/mongo", usersRouter_mongo);
+app.use("/mongo", productsRouter_mongo);
+app.use("/mongo", customersRouter_mongo);
+
+module.exports = app;
---
+++ mApp-backend-master/controllers/mongo/usersController.js
@@ -0,0 +1,14 @@
+const users = require("../../models/mongo/users.js");
+
+exports.insert = async (req, res, next) => {
+    let data = new users({
+        email: "gukatza08147@gmail.com",
+        name: "พุฒิพงศ์",
+        lastname: "โคคา"
+    });
+
+    data.save();
+    res.status(200).json({
+        message: "บันทึกข้อมูลเรียบร้อยแล้ว"
+    });
+}
